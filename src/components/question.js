e6e881ab7731882e271fbe0d480e2203d34a59b6
Daniel <danielosd68@gmail.com>
2022-07-23 11:52:34 +0200

    -

--- src/components/question.js
+++ src/components/question.js
@@ -7,14 +7,18 @@ const Question = (props) => {
     useEffect(() => {
         const question = document.querySelector(`.single-question:nth-child(${props.id})`);
         const answer = document.querySelector(`.single-question:nth-child(${props.id}) > .answer`);
+        const image = document.querySelector(`.single-question:nth-child(${props.id}) > img`);
         if (isClicked === true) {
             question.classList.add('active');
             answer.classList.add('show');
+            console.log(image)
+            image.classList.add('rotated');
         }
 
         else {
             question.classList.remove('active');
             answer.classList.remove('show');
+            image.classList.remove('rotated');
         }
     }, [isClicked])
     return (
