1e741f24bfd0bedc7e9c259ea0398f88695aeeae
Daniel <danielosd68@gmail.com>
2022-07-24 16:07:11 +0200

    Final

--- src/components/main.js
+++ src/components/main.js
@@ -32,16 +32,22 @@ const Main = () => {
     ])
     return (
         <div className="faq-container">
-            <MainPhoto source="images/illustration-woman-online-mobile.svg" alternateText="Mobile image" className="main-illustration mobile" />
-            <div className="section header">
-                <h1>FAQ</h1>
+            <div className="photo">
+                <MainPhoto source={window.innerWidth < 1200 ? "images/illustration-woman-online-mobile.svg" : "images/illustration-woman-online-desktop.svg"} alternateText={window.innerWidth < 1200 ? "Mobile image" : "Desktop image"} className="main-illustration mobile" />
+                {window.innerWidth >= 1200 ? <MainPhoto source="images/illustration-box-desktop.svg" alternateText="Box image" className="main-illustration desktop" /> : <img></img>}
+
             </div>
-            <div className="section questions">
+            <div className="content">
+                <div className="section header">
+                    <h1>FAQ</h1>
+                </div>
+                <div className="section questions">
 
 
-                {questions.map((question, index) => <Question question={question.question} answer={question.answer} key={index + 1} id={question.id} />)}
+                    {questions.map((question, index) => <Question question={question.question} answer={question.answer} key={index + 1} id={question.id} />)}
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
 
